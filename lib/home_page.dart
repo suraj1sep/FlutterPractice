82ee96c3f38454354daa9a26ad1bd6dcc51c0c68
Suraj <suraj.krcse@gmail.com>
2021-06-30 17:50:54 +0530

    Day2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class homepage extends StatelessWidget {
+  int days = 30;
+  String name = "Codepur";
+  double pi = 3.14;
+  bool isMale = true;
+  num temp = 30.5;
+  var day = "Tuesday";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of Flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
-yes
+import 'package:flutter_app/home_page.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: homepage()
+    );
+  }
+}
